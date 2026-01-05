ab9f3077105a91081e3514fea8ace5a123f2d4e4
Layton Sahler <73674208+layton-sahler@users.noreply.github.com>
2026-01-05 12:53:10 -0800

    main files

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    
+    // This matches the URI "SensorApp" set in your CMakeLists.txt
     const QUrl url(u"qrc:/SensorApp/main.qml"_qs);
+
+    // This connection tells you EXACTLY if the loading failed
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QUEUED_CONNECTION);
+
     engine.load(url);
 
     return app.exec();
---
+++ main.qml
@@ -0,0 +1,15 @@
+import QtQuick
+import QtQuick.Controls
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: "SensorApp Interface"
+
+    Text {
+        anchors.centerIn: parent
+        text: "Sensor Data System Online"
+        font.pixelSize: 24
+    }
+}
